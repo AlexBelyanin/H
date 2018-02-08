f5a34fb12a0d5e6be1e9bd9d6be4308f07a75c8e
Eobard <Eobard1SA@gmail.com>
2018-02-08 16:48:44 +0300

    Незначительные изменения в классе TGUI

--- Classes/TGUI.cs
+++ Classes/TGUI.cs
@@ -8,16 +8,29 @@ namespace H.Classes
     {
         //Набор методов для автоматизации создания элементов интерфейса
 
-        static public void SetButton(ref Button inp, string name, int x, int y, EventHandler on_click)
+        static private void SetButton(ref Button inp, string name, int w, int h, int x, int y, EventHandler on_click)
         {
             inp.Location = new Point(x, y);
-            inp.Width = 161;
-            inp.Height = 50;
+            inp.Width = w;
+            inp.Height = h;
             inp.Font = new Font("Segoe Print", 14);
             inp.Text = name;
             inp.Click += on_click;
         }
 
+        static private void SetButton(ref Button inp, string name, int x, int y, EventHandler on_click)
+        {
+            SetButton(ref inp, name, 161, 50, x, y, on_click);
+        }
+
+        /// <summary>
+        /// Возвращает кнопку с указанными параметрами
+        /// </summary>
+        /// <param name="name">Текст на кнопке</param>
+        /// <param name="x">Координата по X</param>
+        /// <param name="y">Координата по Y</param>
+        /// <param name="on_click">Обработчик нажатия</param>
+        /// <returns></returns>
         static public Button GetButton(string name, int x, int y, EventHandler on_click)
         {
             Button btn = new Button();
