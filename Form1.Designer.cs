31f8cf17c82d35a23e817af186ccb8fbe24b375c
Alex <Alex@DESKTOP-77KDD6I>
2018-02-08 03:44:54 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace H
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.main_pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.main_pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // main_pb
+            // 
+            this.main_pb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.main_pb.InitialImage = null;
+            this.main_pb.Location = new System.Drawing.Point(0, -1);
+            this.main_pb.Name = "main_pb";
+            this.main_pb.Size = new System.Drawing.Size(482, 386);
+            this.main_pb.TabIndex = 2;
+            this.main_pb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 384);
+            this.Controls.Add(this.main_pb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.main_pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox main_pb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace H
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            state = State.MainMenu;
+            Draw();
+        }
+
+        public enum State
+        {
+            MainMenu,
+            Options,
+            GameSetup,
+            Game,
+            Battle
+        }
+
+        public State state;
+
+        public void Draw()
+        {
+            //deleting buttons
+            while(this.Controls.Count>1)
+            this.Controls.RemoveAt(1);
+
+            switch (state)
+            {
+                case(State.MainMenu):
+                    main_pb.Image = H.Properties.Resources.Hmm;
+
+                    Button start_bt = new Button();
+                    start_bt.Location = new Point(488, 12);
+                    start_bt.Width = 161;
+                    start_bt.Height = 50;
+                    start_bt.Font = new Font("Segoe Print", 14);
+                    start_bt.Text = "Start";
+                    this.Controls.Add(start_bt);
+                    start_bt.Click += Start_bt_Click;
+
+                    Button options_bt = new Button();
+                    options_bt.Location = new Point(488, 67);
+                    options_bt.Width = 161;
+                    options_bt.Height = 50;
+                    options_bt.Font = new Font("Segoe Print", 14);
+                    options_bt.Text = "Options";
+                    this.Controls.Add(options_bt);
+                    options_bt.Click += Options_bt_Click;
+
+                    Button exit_bt = new Button();
+                    exit_bt.Location = new Point(488, 122);
+                    exit_bt.Width = 161;
+                    exit_bt.Height = 50;
+                    exit_bt.Font = new Font("Segoe Print", 14);
+                    exit_bt.Text = "Exit";
+                    this.Controls.Add(exit_bt);
+                    exit_bt.Click += Exit_bt_Click;
+
+                    break;
+                case (State.Options):
+                    main_pb.Image = H.Properties.Resources.Hop;
+
+                    Button backtomm_bt = new Button();
+                    backtomm_bt.Location = new Point(488, 122);
+                    backtomm_bt.Width = 161;
+                    backtomm_bt.Height = 50;
+                    backtomm_bt.Font = new Font("Segoe Print", 14);
+                    backtomm_bt.Text = "Back";
+                    this.Controls.Add(backtomm_bt);
+                    backtomm_bt.Click += Backtomm_bt_Click;
+
+                    break;
+                case (State.Game):
+                    break;
+                case (State.GameSetup):
+                    main_pb.Image = H.Properties.Resources.Hsg;
+
+                    Button backtomm1_bt = new Button();
+                    backtomm1_bt.Location = new Point(488, 122);
+                    backtomm1_bt.Width = 161;
+                    backtomm1_bt.Height = 50;
+                    backtomm1_bt.Font = new Font("Segoe Print", 14);
+                    backtomm1_bt.Text = "Back";
+                    this.Controls.Add(backtomm1_bt);
+                    backtomm1_bt.Click += Backtomm_bt_Click;
+
+                    break;
+                case (State.Battle):
+                    break;
+
+            }
+            main_pb.Invalidate();
+        }
+
+        private void Backtomm_bt_Click(object sender, EventArgs e)
+        {
+            state = State.MainMenu;
+            Draw();
+        }
+
+        private void Exit_bt_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Options_bt_Click(object sender, EventArgs e)
+        {
+            state = State.Options;
+            Draw();
+        }
+
+        private void Start_bt_Click(object sender, EventArgs e)
+        {
+            state = State.GameSetup;
+            Draw();
+        }
+    }
+}
